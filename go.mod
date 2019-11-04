3e6790103698777288c941724279a05ca4aef86d
Aleksey Kluchnikov <alexs@bone.home>
2019-11-04 11:17:17 +0300

    fix module

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/stofel/testmod
+module github.com/stofel/testmod/v2
 
 go 1.12
--- testmod.go
+++ testmod.go
@@ -2,6 +2,10 @@ package testmod
 
 import "fmt"
 
-func Hi(name string) string {
-	return fmt.Sprintf("Hi %s!", name)
+func Hi(name, lang string) (string, error) {
+  switch lang {
+  case "en": return fmt.Sprintf("Hi %s!", name), nil
+  case "ru": return fmt.Sprintf("Привет %s!", name), nil
+  default  : return "", errors.New("unknown language")
+  }
 }
